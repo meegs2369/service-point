c88393070a0c73855656644bb5ea0781aead6d1a
Matt Eggett <meggett@pointandpay.com>
2019-01-16 11:15:39 -0700

    Clean code

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -6,7 +6,6 @@ public class Palindrome {
 
 	}
 
-
 	public static boolean isPalindrome(String text){
 		String temp  = text.replaceAll("\\s+", "").toLowerCase();
 		return IntStream.range(0, temp.length() / 2)
