36f1043a0b14047fa19b0da7f95f1a4df3bb71a6
Matt Eggett <meegs23@gmail.com>
2020-04-10 18:41:11 -0700

    Updated JavaDoc.

--- src/main/java/com/movedtoatlanta/connectors/data/interfaces/PooledDataConnection.java
+++ src/main/java/com/movedtoatlanta/connectors/data/interfaces/PooledDataConnection.java
@@ -1,7 +1,7 @@
 package com.movedtoatlanta.connectors.data.interfaces;
 
 /**
- * A combination of {@link com.movedtoatlanta.connectors.data.interfaces.DataConnection} and {@link com.movedtoatlanta.connectors.data.interfaces.DataConnection}.
+ * A combination of {@link DataConnection} and {@link ConnectionPool} for pooled Database Connections.
  */
 public interface PooledDataConnection extends DataConnection, ConnectionPool {
 }
